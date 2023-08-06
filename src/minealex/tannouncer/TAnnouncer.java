f58bfdf00b5be0e56d4150423824337eeafc784c
Tect.host <tect.host@gmail.com>
2023-08-06 14:56:57 +0200

    Add files via upload

--- src/minealex/tannouncer/TAnnouncer.java
+++ src/minealex/tannouncer/TAnnouncer.java
@@ -3,12 +3,11 @@ package minealex.tannouncer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import minealex.tannouncer.commands.Commands;
+import minealex.tannouncer.titles.Titles;
 
 import java.util.List;
 
@@ -16,6 +15,7 @@ public class TAnnouncer extends JavaPlugin implements Listener {
 
     private ConfigManager configManager;
     private int taskId;
+    private static int currentTitle = 1; // Variable para mantener el contador de títulos
 
     @Override
     public void onEnable() {
@@ -28,16 +28,28 @@ public class TAnnouncer extends JavaPlugin implements Listener {
         // Registrar el comando /tannouncer reload
         getCommand("tannouncer").setExecutor(new Commands(this));
 
-        // Cargar el tiempo de anuncio desde el archivo announcements.yml
-        int announcementInterval = configManager.getAnnouncementsConfig().getInt("announcement_interval", 10);
+        // Cargar la configuración del título de anuncio desde el archivo announcements.yml
+        Titles.setup(configManager.getAnnouncementsConfig()); // Configurar la instancia de la configuración
 
         // Iniciar la tarea de envío de anuncios cada 10 segundos (o el valor del archivo)
+        int announcementInterval = configManager.getAnnouncementsConfig().getInt("announcement_interval", 10);
         List<String> announcementText = configManager.getAnnouncementsConfig().getStringList("announcement_text");
+        int titleCount = 3; // Cambiar esto al número total de títulos definidos en el archivo announcements.yml
+
         taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
             // Verificar si los anuncios están habilitados en el archivo config.yml
             boolean announcementsEnabled = configManager.getMainConfig().getBoolean("announcements_enabled", true);
 
             if (announcementsEnabled) {
+                // Enviar el título correspondiente en cada intervalo
+                Titles.sendTitleToAllPlayers(configManager.getAnnouncementsConfig(), currentTitle);
+
+                // Incrementar el contador de títulos para el siguiente intervalo
+                currentTitle++;
+                if (currentTitle > titleCount) {
+                    currentTitle = 1;
+                }
+
                 // Enviar el anuncio a todos los jugadores
                 for (String line : announcementText) {
                     for (Player player : Bukkit.getOnlinePlayers()) {
@@ -53,14 +65,5 @@ public class TAnnouncer extends JavaPlugin implements Listener {
     public void onDisable() {
         // Cancelar la tarea de envío de anuncios al desactivar el plugin
         Bukkit.getScheduler().cancelTask(taskId);
-    }
-
-    // Evento para cancelar el chat en la consola
-    @EventHandler
-    public void onPlayerChat(AsyncPlayerChatEvent event) {
-        if (event.getPlayer().hasPermission("tannouncer.bypass")) {
-            return; // Permitir mensajes de jugadores con permiso "tannouncer.bypass"
-        }
-        event.setCancelled(true);
     }
 }
---
+++ src/minealex/tannouncer/titles/Titles.java
@@ -0,0 +1,38 @@
+package minealex.tannouncer.titles;
+
+import net.minecraft.server.v1_8_R3.ChatComponentText;
+import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class Titles {
+
+    public static void sendTitleToAllPlayers(FileConfiguration announcementsConfig, int titleNumber) {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            sendTitle(player, announcementsConfig, titleNumber);
+        }
+    }
+
+    public static void sendTitle(Player player, FileConfiguration announcementsConfig, int titleNumber) {
+        String titlePath = "titulo" + titleNumber;
+        if (announcementsConfig.isConfigurationSection(titlePath)) {
+            ConfigurationSection titleSection = announcementsConfig.getConfigurationSection(titlePath);
+            String titleText = ChatColor.translateAlternateColorCodes('&', titleSection.getString("text"));
+            int fadeIn = titleSection.getInt("fadeIn", 10);
+            int stay = titleSection.getInt("stay", 40);
+            int fadeOut = titleSection.getInt("fadeOut", 10);
+
+            CraftPlayer craftPlayer = (CraftPlayer) player;
+            PacketPlayOutTitle titlePacket = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, new ChatComponentText(titleText), fadeIn, stay, fadeOut);
+            craftPlayer.getHandle().playerConnection.sendPacket(titlePacket);
+        }
+    }
+
+    public static void setup(FileConfiguration announcementsConfig) {
+        // No es necesario hacer nada aquí, ya que ahora enviamos los títulos directamente desde el método onEnable de TAnnouncer.java.
+    }
+}
