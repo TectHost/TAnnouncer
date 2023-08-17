1d94848236bc368e3ae942275bc223362c47fed9
Tect.host <tect.host@gmail.com>
2023-08-17 11:38:41 +0200

    Add files via upload

--- src/minealex/tannouncer/commands/Commands.java
+++ src/minealex/tannouncer/commands/Commands.java
@@ -19,18 +19,21 @@ public class Commands implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("tannouncer")) {
-            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-                if (sender.hasPermission("tannouncer.reload")) {
-                    plugin.reloadConfig();
-                    String reloadMessage = messagesConfig.getString("reload_config", "&aConfig reloaded.");
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', reloadMessage));
-                } else {
-                    String noPermissionMessage = messagesConfig.getString("no_permission", "&cYou don't have permissions to execute this command.");
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', noPermissionMessage));
-                }
-                return true;
+        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+            if (sender.hasPermission("tannouncer.reload")) {
+                plugin.reloadConfig();
+                String reloadMessage = messagesConfig.getString("reload_config", "&5TAnnouncer &e> &aConfig reloaded.");
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', reloadMessage));
+            } else {
+                String noPermissionMessage = messagesConfig.getString("no_permission", "&5TAnnouncer &e> &cYou don't have permission to execute this command.");
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', noPermissionMessage));
             }
+            return true;
+        } else if (args.length == 1 && args[0].equalsIgnoreCase("version")) {
+            String versionMessage = messagesConfig.getString("version_message", "&5TAnnouncer &e> &aTAnnouncer version: &7{{version}}")
+                    .replace("{{version}}", plugin.getDescription().getVersion());
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', versionMessage));
+            return true;
         }
         return false;
     }
